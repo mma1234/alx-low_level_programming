1804abe0d1d15374ed2cf7365be2de0322ae5d5c
mma1234 <beautybenedict68@gmail.com>
2022-09-09 14:45:13 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z';)
+	{
+
+		if (alpha == 'q')
+		{
+			alpha++;
+		}
+		else if (alpha == 'e')
+		{
+			alpha++;
+		}
+		else
+		{
+			putchar(alpha);
+			alpha++;
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
+
