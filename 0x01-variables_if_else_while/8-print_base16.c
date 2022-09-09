508c5fd463b2157cb7dfdad8f30be1ae8a96edca
mma1234 <beautybenedict68@gmail.com>
2022-09-09 15:49:20 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+	char alpha = 'a';
+
+	for (num = '0'; num, <= '9'; num++)
+		putchar(num);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+	return (0);
+}
