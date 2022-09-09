1fb1a9c6ccd60ff9d9aacf02b7dc76ce8acb8e93
mma1234 <beautybenedict68@gmail.com>
2022-09-09 12:39:01 -0700

    Task zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints if number is positive, zero or negative
+ * main - Entry point
  *
  * Return: Always (Success)
  */
@@ -29,6 +29,6 @@ int main(void)
 	}
 
 
-	return (0)
+	return (0);
 }
 
