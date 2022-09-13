ac5b02b2ab3c3b9458ca3d2c072716f20e4042c3
mma1234 <beautybenedict68@gmail.com>
2022-09-13 15:58:46 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-	
+
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
@@ -17,7 +17,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				int prod = j * i;
-				
+
 				if (j == 0)
 				{
 					_putchar('0');
