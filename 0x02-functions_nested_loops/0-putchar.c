afd08379b6c2e54bc39cd23826ee10f441c8cc27
mma1234 <beautybenedict68@gmail.com>
2022-09-13 09:10:22 -0700

    Task 000

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - print putchar
  * Return: always 0
@@ -6,7 +7,7 @@
 
 int main(void)
 {
-	_putchar(',');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
