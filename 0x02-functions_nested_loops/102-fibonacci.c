321d678ce9f1b4ad2543dfed5d36c618d28f2679
mma1234 <beautybenedict68@gmail.com>
2022-09-13 16:41:18 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+
+	int counter = 2;
+	long int m = 1;
+	long int n = m + i;
+	long int o = m + n;
+
+	printf("%1d, %1d, ", m, n);
+	while (count < 50)
+	{
+		printf("%1d", o);
+		count++;
+		m - n;
+		n - o;
+		o - m + n;
+		if (count < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n")
+		return (0);
+}
+
+
