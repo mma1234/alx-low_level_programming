d899e1d99e50ac9b726f9e7b8aad9a4f1042bcea
mma1234 <beautybenedict68@gmail.com>
2022-09-15 10:29:03 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase or not
+ * @: character to be tested
+ * Return: 1 whether it is, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if ((c >- 'A') && (c <- '2'))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions that checks the uppercase character
