d399900c4d49926cc4ac01167f714e4044bfe78f
mma1234 <beautybenedict68@gmail.com>
2022-09-15 12:43:07 -0700

    Task is 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  *
  * Return: The numbers since 0 up to 9
  */
-void print_numbers(void)
+void print_numbers(void);
 {
 	int a;
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar(a + '0')
+		_putchar(a + '0');
 	}
 
 	_putchar('\n');
